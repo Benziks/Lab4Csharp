579b5341bae22163282094150102bedb5962b3da
Benziks <joker.nytik@gmail.com>
2023-10-01 19:03:52 +0300

    Добавьте файлы проекта.

---
+++ Lab4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lab4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector v1 = new Vector(1.3f, 2.5f, 3.6f);
+            Vector v2 = new Vector(4.4f, 5.2f, 6.8f);
+            Vector v3 = new Vector(7.9f, 8.4f, 9.2f);
+           
+            Console.WriteLine("Ваш вектор v1: " + v1.DisplayVector());
+            Console.WriteLine("Ваш вектор v1: " + v2.DisplayVector());
+            Console.WriteLine("Ваш вектор v1: " + v3.DisplayVector());
+
+            //(a+b) x c. "x" Было понято как скалярное произведение %
+            float r = (v1 + v2) % v3;
+            Console.WriteLine($"\nСкалярное произведение v1v2 и v3 = {r}");
+
+            Vector t = (v1 + v3) * v3;
+            Console.WriteLine("\nВектор t = "+t.DisplayVector());
+
+            Console.WriteLine("\nДлина вектора v1 равна: " + Math.Round(v1.VectorLength(),4));
+            Console.WriteLine("Длина вектора v2 равна: " + Math.Round(v2.VectorLength(), 4));
+            Console.WriteLine("Длина вектора v3 равна: " + Math.Round(v3.VectorLength(), 4));
+
+            Vector resultingVector = v1 + v2 + v3;
+            Console.WriteLine("\nВаш результирующий вектор = " + resultingVector.DisplayVector());
+
+            Console.WriteLine("\nДлина результирующего вектора = " + Math.Round(resultingVector.VectorLength(), 5));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab4/Vector.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+
+namespace Lab4
+{
+    internal class Vector
+    {
+        private float _x;
+        private float _y;
+        private float _z;
+
+        public float X { get => _x; set => _x = value; }
+        public float Y { get => _y; set => _y = value; }
+        public float Z { get => _z; set => _z = value; }
+
+        public Vector(float x = 0, float y = 0, float z = 0)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public string DisplayVector()
+        {
+            return X + " " + Y + " " + Z;
+        }
+
+        
+        public double VectorLength()
+        {
+            return Math.Sqrt(X * X + Y * Y + Z * Z);
+        }
+        public static Vector operator +(Vector coordinate1, Vector coordinate2)
+        {
+            return new Vector(coordinate1.X + coordinate2.X, coordinate1.Y + coordinate2.Y, coordinate1.Z + coordinate2.Z);
+        }
+        public static float operator %(Vector coordinate1, Vector coordinate2)
+        {
+            return coordinate1.X * coordinate2.X + coordinate1.Y * coordinate2.Y + coordinate1.Z * coordinate2.Z;
+        }
+        public static Vector operator *(Vector coordinate1, Vector coordinate2)
+        {
+            return new Vector(coordinate1.X * coordinate2.X, coordinate1.Y * coordinate2.Y, coordinate1.Z * coordinate2.Z);
+
+        }
+
+    }
+}
